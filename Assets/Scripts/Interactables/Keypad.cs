18613a54a1b19682df2f5871bc525389ec50c56d
wareverdud <71446364+wareverdud@users.noreply.github.com>
2023-10-11 16:20:39 +0300

    skybox

--- Assets/Scripts/Interactables/Keypad.cs
+++ Assets/Scripts/Interactables/Keypad.cs
@@ -7,17 +7,6 @@ public class Keypad : Interactable
     [SerializeField]
     private GameObject door;
     private bool doorOpen;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     protected override void Interact()
     {
